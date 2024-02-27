91c15e1121fece469139fac05980939f5bc06de3
kallehed <kallehed@gmail.com>
2024-02-27 21:46:46 +0100

    better options

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 type F = f64;
 
 #[derive(Clone, Copy)]
@@ -12,14 +14,15 @@ fn main() {
     // let goto = Pos{x: 0.42884,y: -0.231345};
     let goto = Pos{x: -1.629170093905343, y: -0.0203968};
     // let goto = Pos{x: -0.761574, y:-0.0847596};
+    let mut out = std::io::stdout().lock();
 
     // 78
     for zoom in 1.. {
-        let size = (1.5 as F).powf(-zoom as F);
+        let size = (1.05 as F).powf(-zoom as F);
         println!("size: {}", size);
 
-        let height = 50;
-        let width = 100;
+        let height = 70;
+        let width = 150;
         let mut most_bounces = 0;
         for y in 0..height {
             for x in 0..width {
@@ -27,23 +30,23 @@ fn main() {
                     x: (((x as F / width as F) - 0.5) * 2.0) * size + goto.x,
                     y: (((y as F / height as F) - 0.5) * 2.0) * size + goto.y,
                 }) {
-                    0 => print!(" "),
+                    0 => {out.write(&[b' ']).unwrap();},
                     x => {
                         most_bounces = most_bounces.max(x);
                         // let x = ((x - 1)/1) as u8;
                         // let x = x.saturating_add(33);
                         // let x = x.min(126);
-                        let x = ((x - 1) % (126 - 33 + 1)) as u8;
-                        let x = x.saturating_add(33);
-                         let x = x.min(126);
-                        print!("{}", x as char);
+                        let x = (x % (126 - 33 + 1)) as u8;
+                        let x = x + 33;
+                        let x = x.min(126);
+                        out.write(&[x]).unwrap();
                     },
                 }
             }
             println!();
         }
         println!("max bounces: {}", most_bounces);
-        std::thread::sleep(std::time::Duration::from_millis(100));
+        std::thread::sleep(std::time::Duration::from_millis(40));
     }
 }
 
@@ -55,7 +58,7 @@ fn in_mandelbrot_set(c: Pos) -> i32 {
         }
     }
     let mut z = Pos { x: 0.0, y: 0.0 };
-    for bounce in 1..50000 {
+    for bounce in 1..500 {
         z = f(z, c);
         if z.x * z.x + z.y * z.y >= 4.0 {
             return bounce;
